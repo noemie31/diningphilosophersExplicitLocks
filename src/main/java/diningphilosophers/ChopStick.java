ab7933baf284616f19a5ee05f3bedfeb20f911fe
noemie31 <noemie.lipovac@gmail.com>
2023-09-25 15:39:12 +0200

    verrou fait

--- src/main/java/diningphilosophers/ChopStick.java
+++ src/main/java/diningphilosophers/ChopStick.java
@@ -1,32 +1,31 @@
 package diningphilosophers;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class ChopStick {
 
     private static int stickCount = 0;
     private boolean iAmFree = true;
     private final int myNumber;
 
+    private final Lock verrou = new ReentrantLock();
     public ChopStick() {
         myNumber = ++stickCount;
     }
 
-    synchronized public boolean tryTake(int delay) throws InterruptedException {
-        if (!iAmFree) {
-            wait(delay);
-            if (!iAmFree) // Toujours pas libre, on abandonne
-            {
-                return false; // Echec
-            }
+    public boolean tryTake(int DELAY) throws InterruptedException {
+        if(verrou.tryLock(DELAY, TimeUnit.MILLISECONDS)){
+            System.out.println("Stick " + myNumber + " Taken");
+            return true;
         }
-        iAmFree = false;
-        // Pas utile de faire notifyAll ici, personne n'attend qu'elle soit occupée
-        return true; // Succès
+        return false;
     }
 
-    synchronized public void release() {
-        iAmFree = true;
-        notifyAll();
+    public void release() {
         System.out.println("Stick " + myNumber + " Released");
+        verrou.unlock();
     }
 
     @Override
